fbf61711de784a21b5e94c362323550943f4898c
Mr.Shtein <mr.shtein61@yandex.ru>
2021-05-11 20:15:37 +0300

    Completed firstStage

---
+++ editor/TextArea.java
@@ -0,0 +1,13 @@
+package editor;
+
+import javax.swing.JTextArea;
+
+public class TextArea extends JTextArea {
+
+    public TextArea(int x, int y) {
+        super(x, y);
+        setName("TextArea");
+    }
+
+
+}
--- editor/TextEditor.java
+++ editor/TextEditor.java
@@ -4,9 +4,25 @@ import javax.swing.*;
 
 public class TextEditor extends JFrame {
     public TextEditor() {
+        setTitle("The first stage");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(300, 300);
+        initComponents();
         setVisible(true);
         setLayout(null);
+    }
+
+    private void initComponents() {
+        JPanel common = new JPanel();
+        common.setSize(280, 280);
+        common.setBounds(5,5,100,100);
+
+        JTextArea textArea = new JTextArea();
+
+        common.add(new TextArea(15, 20));
+
+        add(common);
+
+
     }
 }
