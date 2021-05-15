29373a2ccb266d1ef745dc5f1d1ffa1513f4ab85
Mr.Shtein <mr.shtein61@yandex.ru>
2021-05-15 10:34:50 +0300

    Completed firstStage

--- editor/TextEditor.java
+++ editor/TextEditor.java
@@ -1,27 +1,105 @@
 package editor;
 
+import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import javax.swing.*;
 
 public class TextEditor extends JFrame {
+   // final String path = System.getProperty("user.dir") + "/Text Editor/task/";
+
     public TextEditor() {
         setTitle("The first stage");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 300);
+
+        setSize(500, 370);
+        setLocationRelativeTo(null);
+        setLayout(new FlowLayout());
         initComponents();
         setVisible(true);
-        setLayout(null);
     }
 
     private void initComponents() {
-        JPanel common = new JPanel();
-        common.setSize(280, 280);
-        common.setBounds(5,5,100,100);
+        JPanel topPanel = new JPanel();
+        topPanel.setSize(600, 100);
+
+        JTextField textField = new JTextField();
+        textField.setName("FilenameField");
+        textField.setPreferredSize(new Dimension(230, 27));
 
         JTextArea textArea = new JTextArea();
+        textArea.setName("TextArea");
 
-        common.add(new TextArea(15, 20));
+        JButton saveBtn = new JButton("Save");
+        saveBtn.setName("SaveButton");
+        saveBtn.setPreferredSize(new Dimension(100, 27));
+        saveBtn.addActionListener(event -> {
+            String fileName = textField.getText();
+            try {
+                FileWriter fw = new FileWriter( fileName, false);
+                String textToFile = textArea.getText();
+                fw.write(textToFile);
+                fw.close();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        });
 
-        add(common);
+
+
+        JButton loadBtn = new JButton("Load");
+        loadBtn.setName("LoadButton");
+        loadBtn.setPreferredSize(new Dimension(100, 27));
+        loadBtn.addActionListener(event -> {
+            String fileName = textField.getText();
+            try {
+                Path pathToFile = Paths.get(fileName);
+                if (Files.exists(pathToFile)) {
+                    String textFromFile = Files.readString(pathToFile);
+                    textArea.setText(textFromFile);
+                } else {
+                    textArea.setText("");
+                    throw new FileNotFoundException("File not found");
+                }
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        });
+
+
+        topPanel.add(textField);
+        topPanel.add(saveBtn);
+        topPanel.add(loadBtn);
+
+
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setSize(600, 400);
+
+
+
+        JScrollPane scrollPane = new JScrollPane(textArea);
+        scrollPane.setName("ScrollPane");
+        scrollPane.setPreferredSize(new Dimension(440, 250));
+
+
+
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_ALWAYS);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+
+
+
+        bottomPanel.add(scrollPane);
+        add(topPanel);
+        add(bottomPanel);
+
+
 
 
     }
